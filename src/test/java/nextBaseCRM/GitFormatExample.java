c960540aff012aa6c553860b40ce6df5b69bec98
mregall <michael.regall@gmail.com>
2022-06-20 16:31:31 -0500

    Format update

--- src/test/java/nextBaseCRM/GitFormatExample.java
+++ src/test/java/nextBaseCRM/GitFormatExample.java
@@ -3,4 +3,10 @@ package nextBaseCRM;
 public class GitFormatExample {
 
     //Git Commit Formatting
+    //Updates to master require an approval from another person in the group
+    //Please only update your own additions to master
+    //Name convention: Packages are named for the User Story example: .US_1
+    //Classes will be names after the Acceptance Criteria.  example: .AC_1_YourName
+
+    //We can discuss all this further at the next larger group meeting.
 }
