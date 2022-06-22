5644b4c69dabc921ab3aadf7620c62d83fd44c4f
olga1039 <olga-d7@outlook.com>
2022-06-21 22:07:02 -0400

    Olha_US_1 updated

--- src/test/java/nextBaseCRM/Olha_US_1.java
+++ src/test/java/nextBaseCRM/Olha_US_1.java
@@ -1,25 +1,34 @@
 package nextBaseCRM;
 
 import nextBaseCRM.base.TestBase;
+import nextBaseCRM.utilities.CRM_Utilities;
+import nextBaseCRM.utilities.ConfigurationReader;
+import nextBaseCRM.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-public class Olha_US_1 extends TestBase {
+public class Olha_US_1 {
     // Given user navigate to login page
     // When user enters username
     // And user enters password
     // Then user should log in to CRM application
     @Test
     public void AC1Login(){
-        driver.get("https://login2.nextbasecrm.com/");
-      //  CRMLoginUtil.crm_login(driver, "helpdesk57@cybertekschool.com", "UserUser");
-        String actualResult = driver.getTitle();
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        WebElement username = Driver.getDriver().findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        username.sendKeys(ConfigurationReader.getProperty("username"));
+        WebElement password = Driver.getDriver().findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        password.sendKeys(ConfigurationReader.getProperty("password"));
+        WebElement loginBtn = Driver.getDriver().findElement(By.xpath("//input[@class='login-btn']"));
+        loginBtn.click();
+        String actualResult = Driver.getDriver().getTitle();
         String expectedResult = "Portal";
         Assert.assertTrue(actualResult.contains(expectedResult));
 
+        Driver.closeDriver();
     }
 
     // Given user navigate to login page
@@ -28,12 +37,18 @@ public class Olha_US_1 extends TestBase {
     // Then user should not log in to CRM application
     @Test
     public void AC2LoginNegative(){
-        driver.get("https://login2.nextbasecrm.com/");
-       // CRMLoginUtil.crm_login(driver, "hr57@cybertekschool.com", "User");
-        WebElement errorText = driver.findElement(By.cssSelector(".errortext"));
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        WebElement username = Driver.getDriver().findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        username.sendKeys(ConfigurationReader.getProperty("username"));
+        WebElement password = Driver.getDriver().findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        password.sendKeys("User");
+        WebElement loginBtn = Driver.getDriver().findElement(By.xpath("//input[@class='login-btn']"));
+        loginBtn.click();
+        WebElement errorText = Driver.getDriver().findElement(By.cssSelector(".errortext"));
         String expectedMsg = "Incorrect login or password";
         Assert.assertEquals(errorText.getText(), expectedMsg, "Negative login testing failed");
 
+        Driver.closeDriver();
     }
 
     // Given user on login page
@@ -41,8 +56,8 @@ public class Olha_US_1 extends TestBase {
     // Then option Remember me on this computer was checked
     @Test
     public void AC3RememberMeBtn(){
-        driver.get("https://login2.nextbasecrm.com/");
-        WebElement rememberMeBtn = driver.findElement(By.cssSelector("#USER_REMEMBER"));
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        WebElement rememberMeBtn = Driver.getDriver().findElement(By.cssSelector("#USER_REMEMBER"));
         rememberMeBtn.click();
         System.out.println(rememberMeBtn.isSelected());
 
@@ -50,15 +65,10 @@ public class Olha_US_1 extends TestBase {
 
     @Test
     public void AC4ForgotPassword(){
-        driver.get("https://login2.nextbasecrm.com/");
-        WebElement forgotPassword = driver.findElement(By.cssSelector(".login-link-forgot-pass"));
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        WebElement forgotPassword = Driver.getDriver().findElement(By.cssSelector(".login-link-forgot-pass"));
         forgotPassword.click();
-        Assert.assertEquals(driver.getTitle(), "Get Password", "Forgot password verification failed");
-    }
-
-    @AfterMethod
-    public void tearDown(){
-        driver.close();
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Get Password", "Forgot password verification failed");
     }
 
 }
